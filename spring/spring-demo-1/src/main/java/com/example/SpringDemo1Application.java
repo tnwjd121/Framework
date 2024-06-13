817c68e0b4899907dbd479cd5c8ac9a53ba1728d
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2024-06-13 09:30:04 +0900

    spring 생성

---
+++ spring/spring-demo-1/src/main/java/com/example/SpringDemo1Application.java
@@ -0,0 +1,15 @@
+//src/main/java
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo1Application.class, args);
+	}
+
+}
+// 우클릭 > run as >spring boot app
